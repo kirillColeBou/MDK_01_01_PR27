94fa30d2351e897327d733f884247356ad499e00
Kirill <kirillteplycov888@gmail.com>
2023-12-18 23:34:50 +0500

    Реализованы классы

--- Кинотеатры_Тепляков/Classes/CinemaContext.cs
+++ Кинотеатры_Тепляков/Classes/CinemaContext.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Кинотеатры_Тепляков.Models;
 
 namespace Кинотеатры_Тепляков.Classes
 {
-    public class CinemaContext
+    public class CinemaContext : Cinema
     {
+        public CinemaContext(int id, string nameCinema, int countHall, int countPlace) : base(id, nameCinema, countHall, countPlace) { }
+        public static List<CinemaContext> AllCinemas()
+        {
+            List<CinemaContext> allCinemas = new List<CinemaContext>();
+            MySqlConnection connection = Connection.DBConnection.OpenConnection();
+            MySqlDataReader cinemaQuery = Connection.DBConnection.Query("SELECT * FROM Cinemas.cinema", connection);
+            while (cinemaQuery.Read())
+            {
+                allCinemas.Add(new CinemaContext(
+                    cinemaQuery.GetInt32(1),
+                    cinemaQuery.GetString(2),
+                    cinemaQuery.GetInt32(3),
+                    cinemaQuery.GetInt32(4)));
+            }
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+            return allCinemas;
+        }
     }
 }
--- Кинотеатры_Тепляков/Classes/PosterContext.cs
+++ Кинотеатры_Тепляков/Classes/PosterContext.cs
@@ -1,13 +1,33 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Кинотеатры_Тепляков.Models;
 
 namespace Кинотеатры_Тепляков.Classes
 {
-    public class PosterContext
+    public class PosterContext : Poster
     {
-
+        public PosterContext(int id, int idCinema, string nameFilm, string time, string price) : base(id, idCinema, nameFilm, time, price) { }
+        public static List<PosterContext> AllPosters()
+        {
+            List<PosterContext> allPosters = new List<PosterContext>();
+            MySqlConnection connection = Connection.DBConnection.OpenConnection();
+            MySqlDataReader posterQuery = Connection.DBConnection.Query("SELECT * FROM Cinemas.poster", connection);
+            while (posterQuery.Read())
+            {
+                allPosters.Add(new PosterContext(
+                    posterQuery.GetInt32(1),
+                    posterQuery.GetInt32(2),
+                    posterQuery.GetString(3),
+                    posterQuery.GetString(4),
+                    posterQuery.GetString(5)));
+            }
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+            return allPosters;
+        }
     }
 }
